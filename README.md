b59bc0610c24217fcdb96f5c1fd18e3cf68ca612
mojotron <igormalj@gmail.com>
2018-06-12 12:33:05 +0200

    Refactoring stock_picker code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Classic programing problems
+Learning Ruby by solving classic exercises.
+
+## Caeser cipher
+First version solve problem with indexing alphabet list. Second version is done more with math.
+
+## Stock picker
+Takes array of stock prices, one for each day. Method should return pair of days in form
+[best_day_to_buy, best_day_to_sell]. Can't sell before buy.
--- stock_picker.rb
+++ stock_picker.rb
@@ -3,13 +3,11 @@ def stock_picker(input)
 	pairs = []
 	diff = []
 	#remove edge cases, cant sell before buy 
-	if(input.max == input.first)
-		input.delete(input.first)
-	end
+	input.delete(input.first) if input.max == input.first
+	
 	#or sell if you bought last day
-	if(input.min == input.last)
-		input.delete(input.last)
-	end
+	input.delete(input.last) if input.min == input.last
+	
 	#set all combinations pairs [best_buy, best_sell]
 	while(input.size > 0)
 		buy = (input.min)
@@ -21,12 +19,12 @@ def stock_picker(input)
 	end
 	#find pair with best_profit, using index becasue they have same
 	best_profit = diff.index(diff.max)
-	puts "Best day to buy #{days.index(pairs[best_profit][0]) + 1 }."
-	puts "Best day to sell #{days.index(pairs[best_profit][1]) + 1 }."
+	puts "Day #{days.index(pairs[best_profit][0]) + 1 } is best for buying."
+	puts "Day #{days.index(pairs[best_profit][1]) + 1 } is best for selling."
 	puts "It brings profit of $#{diff.max}"
-	return pairs[best_profit]
+	
+	pairs[best_profit]
 end
-
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
 p stock_picker([1,2,11,9])
@@ -34,4 +32,5 @@ p stock_picker([3,11,9,8,2,4,1])
 p stock_picker([45,7,25,16,13,30])
 p stock_picker([3,75,3,45,3,12,75,45,1])
 p stock_picker([45,7,25,16,2,30])
+
 
