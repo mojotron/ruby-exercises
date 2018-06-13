968d2faa5cd8125beb2c5f5b93e65c445aff2ec1
mojotron <igormalj@gmail.com>
2018-06-13 12:44:18 +0200

    Edit substring method

--- substrings.rb
+++ substrings.rb
@@ -3,17 +3,12 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def substrings(words, dictionary)
 	x = words.downcase.split(/\W+/)
 	matches = Hash.new(0)
+	
 	x.each do |word|
-		x = 0
-		while(x < dictionary.size)
-			if(word.include?(dictionary[x]))
-				if(matches.keys.include?(dictionary[x]))
-					matches[dictionary[x]] += 1
-				else	
-					matches[dictionary[x]] = 1
-				end
+		dictionary.each do |d_word|
+			if word.include?(d_word)
+				matches[d_word] += 1
 			end
-		x += 1
 		end
 	end
 	matches
@@ -22,3 +17,4 @@ end
 #p word_in_dictionary?("below", dictionary)#{ "below" => 1, "low" => 1 }
 p substrings("below",dictionary)
 p substrings("Howdy partner, sit down! How's it going?",dictionary)
+p substrings("Howdy partner, Howdy partner, Howdy partner",dictionary)
